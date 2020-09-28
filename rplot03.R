21a2f589d6bacc427a51aaab5d1e352ca937be7f
gtibana <67085847+gtibana@users.noreply.github.com>
2020-09-28 13:20:24 -0500

    Update rplot03.R

--- rplot03.R
+++ rplot03.R
@@ -1,3 +1,7 @@
 library(readr)
 myData <- read_delim("data/household_power_consumption.txt", delim = ";", col_names = T)
-with(myData, plot(myData$Voltage, myData$Global_intensity, pch=20, col="yellow"), abline(lm(myData$Global_intensity ~ myData$Voltage, data = myData), col = "blue"))
+with(myDatafilter, {plot(myDatafilter$newdata, myDatafilter$Sub_metering_1, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)") 
+	lines(myDatafilter$newdata, myDatafilter$Sub_metering_2, col= "red")
+	lines(myDatafilter$newdata, myDatafilter$Sub_metering_3, col= "blue")})
+
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
