2a297976fab02042b55afb44831c734efa9e4ddb
gtibana <67085847+gtibana@users.noreply.github.com>
2020-09-28 11:54:01 -0500

    Update rplot01.R

--- rplot01.R
+++ rplot01.R
@@ -1,3 +1,3 @@
 library(readr)
 myData <- read_delim("data/household_power_consumption.txt", delim = ";", col_names = T)
-with(myData, plot(myData$Global_active_power, myData$Global_reactive_power, pch = 19, xlab = "Global Active Power", ylab = "Global Reactive power", col = "blue", frame = F))
+hist(myData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active power")
