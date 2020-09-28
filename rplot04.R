379db7f5fa4981f844f7e157a8b074bee6406439
gtibana <67085847+gtibana@users.noreply.github.com>
2020-09-28 13:29:19 -0500

    Update rplot04.R

--- rplot04.R
+++ rplot04.R
@@ -1,5 +1,16 @@
 library(readr)
 myData <- read_delim("data/household_power_consumption.txt", delim = ";", col_names = T)
-par(mfrow = c(2,1), mar = c(4,4,2,1))
-> hist(subset(myData, Voltage >240)$Global_intensity, col = "blue", xlab = "Global intensity over Voltage mean")
-> hist(subset(myData, Voltage <= 240)$Global_intensity, col = "green", xlab = "Global intensity under Voltage mean")
+
+par(mfrow=c(2,2), mar=c(5,4,2,1))
+
+plot(myDatafilter$newdata, myDatafilter$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", main = NULL)
+
+plot(myDatafilter$newdata, myDatafilter$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", main = NULL)
+
+with(myDatafilter, {plot(myDatafilter$newdata, myDatafilter$Sub_metering_1, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)") 
+	lines(myDatafilter$newdata, myDatafilter$Sub_metering_2, col= "red")
+	lines(myDatafilter$newdata, myDatafilter$Sub_metering_3, col= "blue")})
+
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+plot(myDatafilter$newdata, myDatafilter$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power", main = NULL)
